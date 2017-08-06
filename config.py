509a24860d03eef1062057c53a749a70c8a6bb9d
= <=>
2017-08-06 19:21:29 +0530

    Secured and added enviroment variable of SECRET_KEY

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class BaseConfig(object):
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = '\xc5\xf5\xaf\x11U9\x01\xdd\xcc\xee\xe6\xba1\x0c)\x9b\xfe^\xbc\x9f\x87\xd1\x99B'
+    SECRET_KEY = os.environ['SECRET_KEY']
 
 
 class DevelopmentConfig(BaseConfig):
