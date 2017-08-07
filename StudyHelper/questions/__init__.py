bf9d77832aad6457889a6c8c32abab89abd5e33d
= <=>
2017-08-07 18:25:35 +0530

    Created create_question function

--- StudyHelper/questions/__init__.py
+++ StudyHelper/questions/__init__.py
@@ -1,5 +1,5 @@
-from flask import jsonify, Blueprint
-from StudyHelper.models import db, Question
+from flask import request, jsonify, Blueprint
+from StudyHelper.models import db, Question, User
 
 questions_module = Blueprint('questions', __name__)
 
@@ -27,3 +27,25 @@ def get_question(question_id):
         return jsonify({'status': 'error', 'error': 'No question found.'})
 
     return jsonify({'status': 'success', 'questions': question.serialize()})
+
+@questions_module.route('/new/', methods=['POST'])
+@questions_module.route('/new', methods=['POST'])
+def create_question():
+    question = request.form.get('question')
+    if question is None:
+        return jsonify({'status': 'error', 'error': 'Question not defined.'})
+
+    user_id = request.form.get('user_id')
+    if user_id is None:
+        return jsonify({'status': 'error', 'error': 'user_id not defined.'})
+
+    user = db.session.query(User).get(user_id)
+    if user is None:
+        return jsonify({'status': 'error', 'error': 'No user found with user_id: ' + str(user_id)})
+
+    new_question = Question(question=question, user=user)
+
+    db.session.add(new_question)
+    db.session.commit()
+
+    return jsonify({'status': 'success', 'question_id': new_question.id})
