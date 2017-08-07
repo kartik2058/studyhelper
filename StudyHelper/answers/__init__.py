ff32872ad8e425e0378ceab7ce048cb766154f21
= <=>
2017-08-07 22:23:29 +0530

    Fixed create_answer function

--- StudyHelper/answers/__init__.py
+++ StudyHelper/answers/__init__.py
@@ -1,6 +1,6 @@
 import datetime
 from flask import request, jsonify, Blueprint
-from StudyHelper.models import db, Answer, Question
+from StudyHelper.models import db, Answer, Question, User
 from StudyHelper import make_error
 
 answers_module = Blueprint('answers', __name__)
@@ -52,7 +52,22 @@ def create_answer(question_id):
     if not answer.strip():
         return make_error('Answer cannot be empty.', 407)
 
-    new_answer = Answer(answer=answer, question=question)
+    user_id = request.form.get('user_id')
+    if user_id is None:
+        return make_error('user_id is not defined.', 408)
+    if not user_id.strip():
+        return make_error('user_id cannot be empty.', 409)
+
+    try:
+        int(user_id)
+    except:
+        return make_error('user_id is invalid.', 410)
+
+    user = db.session.query(User).get(int(user_id))
+    if user is None:
+        return make_error('No user found with user_id: ' + str(user_id), 411)
+
+    new_answer = Answer(answer=answer, question=question, user=user)
     db.session.add(new_answer)
     db.session.commit()
 
@@ -64,17 +79,17 @@ def create_answer(question_id):
 def update_answer(question_id, answer_id):
     question = db.session.query(Question).get(question_id)
     if question is None:
-        return make_error('No question found with id: ' + str(question_id), 408)
+        return make_error('No question found with id: ' + str(question_id), 412)
 
     answer = db.session.query(Answer).get(answer_id)
     if answer is None:
-        return make_error('No answer found with id: ' + str(answer_id), 409)
+        return make_error('No answer found with id: ' + str(answer_id), 413)
 
     new_answer = request.form.get('answer')
     if new_answer is None:
-        return make_error('answer is not defined.', 410)
+        return make_error('answer is not defined.', 414)
     if not new_answer.strip():
-        return make_error('Answer cannot be empty.', 411)
+        return make_error('Answer cannot be empty.', 415)
 
     answer.answer = new_answer
     db.session.commit()
