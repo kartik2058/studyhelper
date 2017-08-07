2645b039237080ae437971d2c93ddeabb52ca8bf
= <=>
2017-08-07 17:15:21 +0530

    Made StudyHelper capable of python3

--- StudyHelper/__init__.py
+++ StudyHelper/__init__.py
@@ -10,8 +10,8 @@ db = SQLAlchemy(app)
 def home():
     return "<h1>Welcome to StudyHelper API Server!</h1><h2>I made this server using Python (Flask) for my StudyHelper app hope you enjoy using my app.</h2><h3>Contact me at: kartik2058@gmail.com</h3>"
 
-from users import users_module
-from subjects import subjects_module
+from StudyHelper.users import users_module
+from StudyHelper.subjects import subjects_module
 
 app.register_blueprint(users_module, url_prefix='/users')
 app.register_blueprint(subjects_module, url_prefix='/subjects')
